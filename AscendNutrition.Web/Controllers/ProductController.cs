bb4a4dde8cb9812fc6ebfbed12e91600bb13b55a
Pavel Petrov <pavel.petrovv01@gmail.com>
2024-11-27 15:25:38 +0200

    Added an empty product controller, added products to the layout

---
+++ AscendNutrition.Web/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AscendNutrition.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
